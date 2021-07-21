c9d98e1497e312058f715ae0900cbc140bea63be
psampedro <psampedro@technisys.com>
2021-07-20 21:34:18 -0500

    Add dependencies

---
+++ src/test/java/com/imperium/chuckjokes/ChuckJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imperium.chuckjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
